3f79fef80cf5253a03b098f269ad1d2f9a8c1c64
dizdaroglu <>
2018-02-14 15:06:49 +0300

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'Javascript: The Good Parts',pages:88},
+        {title: 'Harry Potter',pages:125},
+        {title: 'The Dark Tower',pages:66},
+        {title: 'Eloquent Ruby',pages:854}
+    ]
+}
